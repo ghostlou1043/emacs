4ccf619f6e4246d34226789347376967cab56524
ghostlou1043 <ghostlou1043@gmail.com>
2026-01-28 00:25:11 +0800

    feat: Enable and configure popper popup management

--- lisp/core/core-window.el
+++ lisp/core/core-window.el
@@ -21,25 +21,26 @@
   :config
   (setq shackle-select-reused-windows t)  ; 光标依旧焦点已有窗口
   (setq help-window-select t) ;; 如果想要光标不聚集 *help* 需要将该变量设置为 nil
-  
+
   ;; 符号匹配的缓冲区的主要模式
   ;; 字符串匹配缓冲区的名称
   ;; 通过在键值部分使用键 :regexp t 将其转换为正则表达式匹配
+
   (setq shackle-rules ;; 待定制
         '(
           ;; Right
           (helpful-mode :align right :size 0.5)
           ;; ("^\\*helpful.*" :regexp t :select t :align right :size 0.5)
-          
+
           ("*Help*" :align right :size 0.5)
           ("*eldoc*" :align right :size 0.5)
           ("*gt-result*" :align right :size 0.5)
-          
+
           ;; Below
           ("*Messages*" :align below :size 0.3)
           ("*compilation*" :align below :size 0.3)
           ("*Warnings*" :align below :size 0.3)
-          
+
           ;; Shell
           ("*shell*" :align below :size 0.3 :popup t)
           ("*eshell*" :align below :size 0.3 :popup t)
@@ -47,6 +48,7 @@
           ("*vterm*" :align below :size 0.3 :popup t) ;; 必须要 popup t 才有效
           ("*eat*" :align below :size 0.3 :popup t)
           ))
+
   ;; 匹配未在 shackle-rules 中匹配到的 buffer
   ;; 暂时不配置保持默认
   ;; (setq shackle-default-rule
@@ -54,49 +56,67 @@
 
   (shackle-mode +1))
 
-;; (use-package popper
-;;   :ensure t
-;;   :after (shackle)
-;;   ;; :bind
-;;   ;; (:map global-map
-;;   ;;       ("C-;" . popper-toggle)
-;;   ;;       ("C-'" . popper-cycle)
-;;   ;;       ("C-:" . popper-toggle-type))
-;;   :config
-;;   (setq popper-reference-buffers
-;;         '("\\*Messages\\*"                 ; 匹配 *Messages* buffer
-;;           ;; ;; ("Output\\*$" . hide)           ; 匹配以 Output* 结尾的 buffer, 并且抑制了弹窗
-;;           "\\*gt-result\\*"
-;;           "\\*Help\\*"
-;;           help-mode
-;;           "^\\*helpful.*\\*$"
-;;           helpful-mode
-;;           "\\*eldoc\\*"
-;;           compilation-mode                 ; 所有 compilation-mode 的 buffers
-;;           "^\\*eshell.*\\*$"
-;;           eshell-mode
-;;           "^\\*shell.*\\*$"
-;;           shell-mode
-;;           "^\\*term.*\\*$"
-;;           term-mode
-;;           "^\\*eat.*\\*$"
-;;           eat-mode
-;;           "^\\*vterm.*\\*$"
-;;           vterm-mode
-;;           ))  ; 匹配 vterm buffers
-;;   ;; (setq popper-group-function #'popper-group-by-perspective)
-;;   (setq popper-group-function #'popper-group-by-projectile)
-;;   (setq popper-echo-dispatch-keys '(?q ?w ?e ?r ?t ?y ?u ?i ?o ?p))
-;;   (setq popper-echo-lines 2)  ; 设置为 2 行显示
-;;   (setq popper-echo-transform-function
-;;         (lambda (name)
-;;           (if (> (length name) 10)
-;;               (concat (substring name 0 10) "…") ; 截断长名称
-;;             name)))  ; 保留短名称
-;;   ;; (setq popper-mode-line nil) ;; 禁止 mode-line 显示 pop
-;;   (setq popper-display-control nil) ;; 使用 Shackle 控制窗口的弹出
-;;   (popper-mode +1)
-;;   (popper-echo-mode +1))
+(use-package popper
+  :ensure t
+  :after (shackle)
+  :bind
+  (:map global-map
+        ("C-z" . popper-toggle) ;; EAT 吞 C-z , 利用 meow 的 q 键退出，配置稳定后尝试修复
+        ("M-z" . popper-toggle-type)
+        ;; 使用 popper-kill-latest-popup 关闭已打开的弹窗缓冲区
+        ("C-x w p" . popper-cycle)
+        ("C-x w k" . popper-kill-latest-popup))
+  :config
+  (setq popper-display-control nil) ;; 使用 Shackle 控制窗口的弹出
+  (setq popper-group-function #'popper-group-by-projectile) ;; 按照项目分组弹窗
+  ;; (setq popper-mode-line nil) ;; 禁止 mode-line 显示 pop
+  ;; (setq popper-echo-dispatch-keys nil) ;; 不建议使用按键跳转 至少不要是字母
+  (setq popper-echo-dispatch-keys '("0" "1" "2" "3" "4" "5" "6" "7" "8" "9"))
+
+  ;; echo 的显示行数及截断
+  (setq popper-echo-lines 1)  ; 设置为 2 行显示
+  (setq popper-echo-transform-function
+        (lambda (name)
+          (if (> (length name) 10)
+              (concat (substring name 0 10) "…") ; 截断长名称
+            name)))  ; 保留短名称
+  
+
+  (setq popper-reference-buffers
+        '("\\*Messages\\*"                 ; 匹配 *Messages* buffer
+          ;; ("Output\\*$" . hide)           ; 匹配以 Output* 结尾的 buffer, 并且抑制了弹窗，后续也可正常调出
+          "Output\\*$"           ; 匹配以 Output* 结尾的 buffer
+          "\\*gt-result\\*"
+          "\\*Help\\*"
+          help-mode
+          "^\\*helpful.*\\*$"
+          helpful-mode
+          "\\*eldoc\\*"
+          compilation-mode                 ; 所有 compilation-mode 的 buffers
+          ))
+
+  ;; Match eat, eshell, shell, term and/or vterm buffers
+  (setq popper-reference-buffers
+        (append popper-reference-buffers
+                '("^\\*eat.*\\*$"    eat-mode
+                  "^\\*term.*\\*$"   term-mode   ;term as a popup
+                  "^\\*vterm.*\\*$"  vterm-mode  ;vterm as a popup
+                  "^\\*shell.*\\*$"  shell-mode  ;shell as a popup
+                  "^\\*eshell.*\\*$" eshell-mode ;eshell as a popup
+                  )))
+
+  ;; Async Shell Command 存在输出时才弹窗
+  ;; (defun popper-shell-output-empty-p (buf)
+  ;;   (and (string-match-p "\\*Async Shell Command\\*" (buffer-name buf))
+  ;;        (= (buffer-size buf) 0)))
+  ;; 
+  ;; (add-to-list 'popper-reference-buffers
+  ;;              '(popper-shell-output-empty-p . hide))
+
+  
+
+  (popper-mode +1)
+  (popper-echo-mode +1))
 
 (provide 'core-window)
 
