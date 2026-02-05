95172313f3313b3014ff50e95393fcbb7936c399
ghostlou1043 <ghostlou1043@gmail.com>
2026-02-05 15:57:47 +0800

    refactor: Configure lsp-proxy for org-babel support

--- lisp/init-lsp-proxy.el
+++ lisp/init-lsp-proxy.el
@@ -6,16 +6,16 @@
   :ensure (lsp-proxy
            :host github
            :repo "jadestrong/lsp-proxy"
-           :files ("*.el"))
+           :files ("*.el")
+           :build (:not compile))
   :if(1043/enable-lsp-proxy-p)
   :bind (:map lsp-proxy-mode-map
               ("M-." . lsp-proxy-find-definition)
-              ("M-?" . lsp-proxy-find-references)
-              )
-  :init
-  (setq lsp-proxy--exec-file
-        (expand-file-name "lsp-proxy/node_modules/.bin/emacs-lsp-proxy" user-emacs-directory))
-  :hook ((python-mode)
+              ("M-?" . lsp-proxy-find-references))
+
+
+  :hook ((org-mode)
+         (python-mode)
          (python-ts-mode)
          (tsx-ts-mode)
          (js-ts-mode)
@@ -23,10 +23,30 @@
          (typescript-ts-mode))
 
   :config
+  (setq lsp-proxy-server-path
+        (expand-file-name "lsp-proxy/node_modules/.bin/emacs-lsp-proxy" user-emacs-directory))
+
+  ;; Enable LSP support in org-babel code blocks
+  (setq lsp-proxy-enable-org-babel t)
+
+  ;; Enable LSP support in org-edit-special buffers (default: t)
+  (setq lsp-proxy-org-edit-special-enable-lsp t)
+
+  ;; Specify which languages to enable LSP support for in org-babel blocks
+  (setq lsp-proxy-org-babel-enabled-languages
+        '("python" "typescript" "javascript" "tsx" "bash" "rust" "go"))
+
+  ;; Map org-babel language names to LSP language IDs
+  (setq lsp-proxy-org-babel-language-map
+        '(("shell" . "bash")
+          ("sh" . "bash")
+          ("tsx-ts" . "tsx")
+          ("typescript-ts" . "typescript")))
+
+  ;; 日志信息记录等级
   (setq lsp-proxy-log-level 0)
+  ;; 选择提供诊断信息的包
   (setq lsp-proxy-diagnostics-provider :auto))
-
-
 
 (provide 'init-lsp-proxy)
 
